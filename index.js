1e70d3b04e9e7e56eb8a648375ab3c151d974fe8
ReTims <renatatims@gmail.com>
2022-09-26 12:44:36 -0500

    Created an array of questions + function userInput

--- index.js
+++ index.js
@@ -1,7 +1,80 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please describe your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Please describe the steps and tools for the installation of your application',
+        name: 'install'
+    },
+   
+    {
+        type: 'input',
+        message: 'Please describe the usage of your application',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Please describe the Features in your application',
+        name: 'features'
+    },
+    {
+        type: 'list',
+        message: 'Please select a License',
+        choices: ['Apache License 2.0', 'Boost Software License 1.0', 'Eclipse Public License 2.0','MIT License', 'Mozilla Public License 2.0'], 
+        name: 'license'
+    },
+
+    {
+        type: 'input',
+        message: 'Please enter contributers and how to contribute with your project',
+        name: 'contributers'
+    },
+
+    {
+        type: 'input',
+        message: 'Please describe the steps on how your application can be tested',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your Github username',
+        name: 'github'
+    },
+
+    {
+        type: 'input',
+        message: 'Please enter your e-mail address',
+        name: 'email'
+    },
+
+    {
+        type: 'input',
+        message: 'Credits',
+        name: 'credits'
+    },
+
+    {
+        type: 'input',
+        message: 'Please enter a name to save this file',
+        name: 'fileName'
+    },
+];
+
+function userInput(){
+	return inquirer.prompt(questions);}
+
+userInput()
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
